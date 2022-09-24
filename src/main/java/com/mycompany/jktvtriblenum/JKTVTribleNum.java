40e078b186ad05726d5350a1bf211c815bbd118d
LordEsDeath <kostjakolp@gmail.com>
2022-09-24 18:37:09 +0300

    первая и вторая часть

--- src/main/java/com/mycompany/jktvtriblenum/JKTVTribleNum.java
+++ src/main/java/com/mycompany/jktvtriblenum/JKTVTribleNum.java
@@ -14,13 +14,21 @@ import java.util.Scanner;
 public class JKTVTribleNum {
 
     public static void main(String[] args) {
-        /*X - Будет число которое мы вводим*/
+        /*A - Будет число которое мы вводим*/
         System.out.println("Здравствуй. Набери рандомное трёх-значное число: ");
         Scanner scan = new Scanner(System.in);
         /*Из числа делаем переменую*/
         int a = scan.nextInt();
+        int sum = 0;
         System.out.println("Вы ввели число: " + a);
         System.out.println("Единица: " + a % 10);/*Последнее число*/
         System.out.println("Десятков: " + a / 10 %10);/*Второе число число*/
+        System.out.println("-------------------------------------------");
+        /*Вторая часть*/
+        /*Ищем сумму*/
+        if ( a>0 ){
+            sum = ( a % 10)+( a / 10 % 10 );
+        }
+        System.out.println("Сумма чисел равна: " + sum);
     }
 }
